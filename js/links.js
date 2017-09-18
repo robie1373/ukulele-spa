6b9ce28f57d80b78f5bc7ba9bfa414f0a1aeb274
Robie Lutsey <robie@robiembp.local>
2017-09-18 09:44:49 -0400

    able to receive data from uke api and display it in raw json in the page using fetch() with CORS enabled

--- js/links.js
+++ js/links.js
@@ -3,28 +3,54 @@ import React from 'react';
 class LinksList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {links: []};
+    this.state = {links: [{"link": "https://cnn.com", "linktext": "like news"}]};
   }
+
+
 
   componentDidMount() {
-    const url = `https://uomlb17ija.execute-api.us-east-1.amazonaws.com/dev/uke`
-    const url2 = `https://uomlb17ija.execute-api.us-east-1.amazonaws.com/dev/uke`
-    fetch(url, {mode: 'cors'})
-    .then(result=>result.json())
-    .then(links=>this.setState({links}))
-    .catch((e) => console.log(e))
+    function handleErrors(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response;
+    }
 
-    console.log(this.state.links[0]);
+    function logResponse(response) {
+      console.log('StatusCode: ' + response.status);
+      console.log('Headers: ' + response.headers.get('Content-Type'));
+      console.log('Body: ' + response.body['Items']);
+      Promise.all([response.status, response.headers.get('Content-Type'), response.headers.get('Access-Control-Allow-Origin'), response.body]).then(values => {
+        console.log(values);
+      });
+      return response;
+    }
+
+    const url = `https://uomlb17ija.execute-api.us-east-1.amazonaws.com/dev/uke`
+
+    fetch(url, {mode: 'cors'})
+    .then(handleErrors)
+    .then(logResponse)
+    .then((response) => {
+        if (response.status != 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }
+        return response.json()
+      })
+    .then((data) => {
+      this.setState(data);
+    })
+    .catch((err) => {
+      console.log('Fetch Error: ', err);
+    });
+
   }
+
 
   render() {
     return(
-      <section className="section section--alignCentered background--uke">
-        <h1 className="color--darkgrey section__heading--largest">Links List Placeholder text</h1>
-        <ul>
-         {this.state.links.map(link=><li key={link.id}><a href="{link.link}">{link.linktext}</a></li>)}
-        </ul>
-      </section>
+      <section className="section section--alignCentered background--uke">{JSON.stringify(this.state)}</section>
     )
   }
 }
